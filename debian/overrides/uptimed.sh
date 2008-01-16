9136e263e020cd55077c491e06cbf4b1829b5027
Petter Reinholdtsen <pere@debian.org>
2008-01-16 23:29:09 +0100

    Import Debian changes 1.10.0-5
    insserv (1.10.0-5) unstable; urgency=low
    
      * Made sure to initialize the badstate variable before it is used in
        update-bootsystem-insserv.
      * Removed override files for uptimed and uptimed.sh, as the uptimed
        package now include the LSB headers.
      * Make sure to give a proper error message when failing to enable
        dependency based boot system and insserv return an error code
        (Closes: #461141).

--- debian/overrides/uptimed.sh
+++
@@ -1,8 +0,0 @@
-### BEGIN INIT INFO
-# Provides:          uptimed-init
-# Required-Start:    $remote_fs
-# Required-Stop:
-# Default-Start:     S
-# Default-Stop:
-### END INIT INFO
-# Reported to BTS as #460058
